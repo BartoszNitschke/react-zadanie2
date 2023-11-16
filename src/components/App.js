4db76ccc97148e98d5f8d060fe1388c23a877091
BartoszNitschke <s25771@pjwstk.edu.pl>
2023-11-16 16:42:36 +0100

    full ap

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
+import Header from "./Header.js";
+import Navbar from "./Navbar.js";
+import Article from "./Article.js";
+import Section from "./Section.js";
+import data from "../data/data.json";
+import Footer from "./Footer.js";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello world</h1>
+        <Header></Header>
+        <Navbar></Navbar>
+        <Article></Article>
+        <Section data={data}></Section>
+        <Footer></Footer>
       </div>
     );
   }
